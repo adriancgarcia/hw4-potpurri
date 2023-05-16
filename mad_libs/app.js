a2b363801365c366b6fc66c773ada56d5ba4a4c1
adriancgarcia <adriancgarcia@gmail.com>
2023-05-15 22:22:56 -0500

    mad libs completed.

--- mad_libs/app.js
+++ mad_libs/app.js
@@ -1,15 +1,29 @@
 console.log("Mad Libs!")
 
-words = {
-    number= 
-    adj =
-    pluralNoun =
-    adverb =
-    anotherAdj=
+const words = {
+    number: '',
+    adj: '',
+    pluralNoun: '', 
+    adverb: '',
+    anotherAdj: ''
 }
 
 const startMadLib = () => {
-    const answer = prompt ('what is your favorite number?');
-    console.log(answer)
-    prompt (answer)
+    words.number = prompt ('what is your favorite number?');
+    words.adj = prompt ('One word that best describes you?');
+    words.pluralNoun = prompt ('What do you collect?');
+    words.adverb = prompt ('Enter an adverb');
+    words.anotherAdj = prompt ('What is one word your friends would use to describe you?')
+    console.log(words)
 }
+
+startMadLib()
+
+
+
+// Create the Tech Startup MadLib Story!
+// Now that we've accepted the user's input and saved it to the object, we can use those values to create a MadLib!
+
+// Use the following story and concatenate or interpolate the values from the object into it.
+
+words.techStartup = alert (`Once upon a time a group of ${words.number} General Assembly graduates got together and made a startup called ${words.adj} Technologies. Their goal was to create smart ${words.pluralNoun}. They approached the challenge ${words.adverb} which ultimately lead them to ${words.anotherAdj} fame.`)
